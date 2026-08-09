c875c9ab57070c78821dcd1debe23b2a27380987
Halo <hk332@duke.edu>
2026-08-08 20:49:27 -0400

    Add: longest-substring-without-duplicates - submission-3

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-3.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        HashSet<Character> seen = new HashSet<>(); //track letters seen already
+        // dynamic sliding window
+        int l = 0, maxLength = 0;
+        // add first char to hash
+        if(s.length() == 0) {return 0;}
+        if(s.length() == 1) {return 1;}
+        seen.add(s.charAt(0));
+        for (int r = 1; r < s.length(); r++) {
+            // check if right character is duplicate, increment l if yes
+            while ((seen.contains(s.charAt(r)))) {
+                seen.remove(s.charAt(l));
+                l++;
+            }
+            seen.add(s.charAt(r));
+            maxLength = Math.max(maxLength, r - l + 1); // ensure + 1 to length
+        }
+        return maxLength;
+    }
+}
